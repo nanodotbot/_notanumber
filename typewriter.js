8411a9ad35ecfbc2b42cca81853c02c473034ddf
nanodotbot <nanodotbot@gmail.com>
2023-11-17 17:15:30 +0100

    old design refractured

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,5 @@
 const title = document.getElementById('title');
-const texts = ['Webentwickler', 'Frontend', 'Backend' ,'Fullstack'];
+const texts = ['Webentwickler', 'Frontend', 'Backend' ,'Fullstack', 'NaN'];
 
 let metaindex = 0;
 let index = 0;
